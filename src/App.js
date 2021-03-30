5f4648f000d048ab10b363dcf57d56891d93c01b
Misbahuddin Adil Syed <syedadil010@gmail.com>
2021-03-30 15:34:08 -0400

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
